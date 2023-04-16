e5b9453e638006d62d6c607fcd6a376c8f2d2fc0
Mentis21954 <it21954@hua.gr>
2023-04-16 18:57:02 +0300

    ok

--- transform.py
+++ transform.py
@@ -12,22 +12,22 @@ def clean_the_text(content: dict):
 
     return content_df
 
-def merge_titles_data(releases: dict, listeners: dict):  
-  releases_df = pd.DataFrame(releases)
-  listeners_df = pd.DataFrame(listeners)
-  df = pd.merge(releases_df, listeners_df, on='Title')
-  print('Merge releases and listeners data')
+def merge_titles_data(releases: dict, listeners: dict):
+    releases_df = pd.DataFrame(releases)
+    listeners_df = pd.DataFrame(listeners)
+    df = pd.merge(releases_df, listeners_df, on='Title')
+    print('Merge releases and listeners data')
   
-  return df
+    return df
 
 
 def remove_wrong_values(df):
     # find and remove the rows/titles where there are no selling prices in discogs.com
     df = df[df['Discogs Price'].notna()]
-    print('Remove releases where have wrong year value in discogs.com')
+    print('Remove releases where there no selling price in discogs.com')
     # keep only the rows has positive value of year
     df = df[df['Year'] > 0]
-    print('Remove releases where there no selling price in discogs.com')
+    print('Remove releases where have wrong year value in discogs.com')
     
     return df
 
